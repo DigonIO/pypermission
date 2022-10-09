f46810859d779ac3a48c7624cee2f3e777f9af5f
Jendrik A. Potyka <jpotyka@digon.io>
2022-10-09 16:57:13 +0200

    init sqlalchemy based persistency

---
+++ src/pypermission/sqlalchemy/core.py
@@ -0,0 +1,100 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sqlalchemy.orm.session import sessionmaker
+from sqlalchemy.engine.base import Engine
+from sqlalchemy.exc import IntegrityError
+
+from pypermission.core import Authority as _Authority
+from pypermission.core import EntityID, Permission, PermissionMap, PermissionNode
+from pypermission.sqlalchemy.models import (
+    ENTITY_ID_MAX_LENGHT,
+    SERIAL_ENTITY_ID_LENGHT,
+    DeclarativeMeta,
+    SubjectEntry,
+)
+from pypermission.error import EntityIDCollisionError, UnknownSubjectIDError
+
+
+class Authority(_Authority):
+
+    _engine: Engine
+    _session_maker: sessionmaker
+
+    def __init__(self, *, engine: Engine, nodes: type[PermissionNode] | None = None) -> None:
+        super().__init__(nodes=nodes)
+
+        self._session_maker = sessionmaker(
+            bind=engine,
+            autocommit=False,
+            autoflush=False,
+        )
+
+        DeclarativeMeta.metadata.create_all(bind=engine)
+
+    def add_subject(self, sid: EntityID, db: Session | None = None) -> None:
+        """Create a new subject for a given ID."""
+        serial_eid = entity_id_serializer(sid)
+        db = self._setup_db_session(db)
+
+        subject_entry = SubjectEntry(eid=serial_eid)
+        db.add(subject_entry)
+
+        try:
+            db.commit()
+        except IntegrityError as err:
+            raise EntityIDCollisionError from None  # TODO
+
+    def rem_subject(self, sid: EntityID, db: Session | None = None) -> None:
+        """Remove a subject for a given ID."""
+        serial_eid = entity_id_serializer(sid)
+        db = self._setup_db_session(db)
+
+        subject_entry = db.query(SubjectEntry).get(serial_eid)
+        if subject_entry is None:
+            raise UnknownSubjectIDError  # TODO
+
+        db.delete(subject_entry)
+        db.commit()
+
+    def get_subjects(self, db: Session | None = None) -> set[EntityID]:
+        """Get the IDs for all known subjects."""
+        db = self._setup_db_session(db)
+
+        subject_entries = db.query(SubjectEntry).all()
+        return set(entity_id_deserializer(entry.eid) for entry in subject_entries)
+
+    def _setup_db_session(self, db: Session | None) -> Session:
+        if db is None:
+            return self._session_maker()
+        return db
+
+
+def entity_id_serializer(eid: EntityID) -> str:
+    if isinstance(eid, int):
+        serial_type = "int"
+        serial_eid = str(eid)
+    elif isinstance(eid, str):
+        serial_type = "str"
+        serial_eid = eid
+    else:
+        raise ValueError  # TODO
+
+    if len(serial_eid) > ENTITY_ID_MAX_LENGHT:
+        raise ValueError  # TODO
+
+    return f"{serial_eid}:{serial_type}"
+
+
+def entity_id_deserializer(serial_eid: str) -> EntityID:
+    if len(serial_eid) > (SERIAL_ENTITY_ID_LENGHT):
+        raise ValueError  # TODO
+
+    serial_type = serial_eid[-3:]
+    serial_eid = serial_eid[:-4]
+
+    if serial_type == "int":
+        return int(serial_eid)
+    elif serial_type == "str":
+        return serial_eid
+    else:
+        raise ValueError  # TODO
---
+++ src/pypermission/sqlalchemy/models.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+
+from sqlalchemy import Column, DateTime, event, String
+from sqlalchemy.orm import declarative_base
+
+
+EXTEND_EXISTING = True
+ENTITY_ID_MAX_LENGHT = 100
+SERIAL_ENTITY_ID_LENGHT = ENTITY_ID_MAX_LENGHT + 4
+
+DeclarativeMeta: type = declarative_base()
+
+
+class TimeStampMixin(object):
+    """Timestamping mixin"""
+
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=None)
+    # NOTE maybe this a better solution https://stackoverflow.com/questions/3923910/sqlalchemy-move-mixin-columns-to-end
+    created_at._creation_order = 9998
+    updated_at._creation_order = 9998
+
+    @staticmethod
+    def _updated_at(mapper, connection, target):
+        target.updated_at = datetime.utcnow()
+
+    @classmethod
+    def __declare_last__(cls):
+        event.listen(cls, "before_update", cls._updated_at)
+
+
+class PermissionableEntityMixin(TimeStampMixin):
+    """Permissionable entity mixin"""
+
+    eid = Column(String(length=SERIAL_ENTITY_ID_LENGHT), primary_key=True)  # Entity ID
+
+
+class SubjectEntry(DeclarativeMeta, PermissionableEntityMixin):
+    __tablename__ = "token_table"
+    __table_args__ = {"extend_existing": EXTEND_EXISTING}
