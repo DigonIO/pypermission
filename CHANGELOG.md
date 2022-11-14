bd70842308b3748750ee2d4ef5cd189f3af43865
Jendrik A. Potyka <jpotyka@digon.io>
2022-11-14 18:41:23 +0100

    Add fpreiss as author + fix CHANGELOG
    Co-authored-by: Fabian Preiß <fpreiss@digon.io>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,4 +2,4 @@
 
 ## 0.1.0
 
-+ Initial beta release
++ Initial alpha release
--- src/pypermission/__init__.py
+++ src/pypermission/__init__.py
@@ -1,10 +1,10 @@
 """
-A node based permission engine for python.
+A role-based access control (RBAC) permission library for python.
 
-Author: Jendrik A. Potyka
+Author: Jendrik A. Potyka, Fabian A. Preiß
 """
 
 __version__ = "0.1.0"
-__author__ = "Jendrik A. Potyka"
+__author__ = "Jendrik A. Potyka, Fabian A. Preiß"
 
 from pypermission.core import NodeMap, PermissionNode
